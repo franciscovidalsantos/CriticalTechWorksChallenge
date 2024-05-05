fa5c25d4646252355ff7e49e78ddd2f4c6601d7c
francisco <franciscovidalsantos@gmail.com>
2024-05-05 22:09:14 +0100

    Story 2: added a back navigation click case
    - overrided onOptionsItemSelected in order to prepare navigate back scenarios

--- app/src/main/java/com/example/criticaltechworkschallenge/MainActivity.kt
+++ app/src/main/java/com/example/criticaltechworkschallenge/MainActivity.kt
@@ -1,13 +1,14 @@
 package com.example.criticaltechworkschallenge
 
 import android.os.Bundle
-import com.google.android.material.bottomnavigation.BottomNavigationView
+import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
 import com.example.criticaltechworkschallenge.databinding.ActivityMainBinding
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
 
@@ -31,5 +32,15 @@ class MainActivity : AppCompatActivity() {
         )
         setupActionBarWithNavController(navController, appBarConfiguration)
         navView.setupWithNavController(navController)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            android.R.id.home -> {
+                onBackPressedDispatcher.onBackPressed()
+                return true
+            }
+        }
+        return super.onOptionsItemSelected(item)
     }
 }
